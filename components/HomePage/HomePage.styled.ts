815f875ad359fcf545217301e441d3008ddd1c42
Adam Fahrizal <aylaciprut02@gmail.com>
2024-04-17 13:16:13 +0700

    tugas akhir

---
+++ components/HomePage/HomePage.styled.ts
@@ -0,0 +1,12 @@
+import styled from "styled-components";
+
+export const Header = styled.div`
+    width: 935px;
+    height: auto;
+`
+
+export const Body = styled.div`
+    width: 935px;
+    height: auto;
+`
+
---
+++ components/HomePage/HomePage.tsx
@@ -0,0 +1,18 @@
+import React from "react";
+import {
+    Header,
+    Body,
+} from './HomePage.styled';
+
+export default function HomePage() {
+ return(
+    <>
+        <Header>
+
+        </Header>
+        <Body>
+            
+        </Body>
+    </>
+ );
+}
